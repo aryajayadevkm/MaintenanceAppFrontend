ac54bfbc7fd1511295679a8866084a78dab778c3
aryajayadevkm <aryajayadevkm@gmail.com>
2020-10-06 17:01:12 +0530

    modal fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "./bulma-0.9.0/css/bulma.css";
+import Navbar from "./components/layout/Navbar";
+import Sidenav from "./components/layout/Sidenav";
+import Home from "./components/pages/Home";
+import Dues from "./components/pages/DuesPage";
+import Titlebar from "./components/layout/Titlebar";
+import DuesState from "./context/dues/DuesState";
 
+import "./App.sass";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="tile is-ancestor">
+          <div className="tile is-2 has-background-black-bis">
+            <Sidenav />
+          </div>
+          <div className="tile is-vertical">
+            <Titlebar />
+            <div className="tile is-parent">
+              <div className="tile is-child">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <DuesState>
+                    <Route exact path="/dues" component={Dues} />
+                  </DuesState>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <div class="modal is-active">
+//       <div class="modal-background"></div>
+//       <div class="modal-card">
+//         <header class="modal-card-head">
+//           <p class="modal-card-title">Modal title</p>
+//           <button class="delete" aria-label="close"></button>
+//         </header>
+//         <section class="modal-card-body"></section>
+//         <footer class="modal-card-foot">
+//           <button class="button is-success">Save changes</button>
+//           <button class="button">Cancel</button>
+//         </footer>
+//       </div>
+//     </div>
+//   );
+// };
+// export default App;
